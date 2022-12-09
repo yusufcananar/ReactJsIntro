fccd60dfddc1b6d1dfe77595ef1fc43c1beaf245
Fjolnirr <yusufcan2617@gmail.com>
2022-12-09 14:57:55 +0300

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import CategoryList from './CategoryList';
+import Navi from './Navi'
+import ProductList from './ProductList';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navi></Navi>
+      <CategoryList></CategoryList>
+      <ProductList></ProductList>
     </div>
   );
 }
