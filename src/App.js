b9554ce937e50b2bddccc425f2798aeb6cbcb227
Fjolnirr <yusufcan2617@gmail.com>
2022-12-09 16:46:34 +0300

    Events added.

--- src/App.js
+++ src/App.js
@@ -1,30 +1,40 @@
-import React from "react";
+import React, { Component } from "react";
 import { Col, Container, Row } from "reactstrap";
 import CategoryList from "./CategoryList";
 import Navi from "./Navi";
 import ProductList from "./ProductList";
 
-function App() {
-  let productInfo = {title : "Product List"};
-  let categoryInfo = {title : "Category List"};
+export default class App extends Component {
+  productInfo = { title: "Product List" };
+  categoryInfo = { title: "Category List" };
 
-  return (
-    <div>
-      <Container>
-        <Row>
-          <Navi />
-        </Row>
-        <Row>
-          <Col xs="3">
-            <CategoryList info={categoryInfo}/>
-          </Col>
-          <Col xs="9">
-            <ProductList info={productInfo} />
-          </Col>
-        </Row>
-      </Container>
-    </div>
-  );
+  state = { currentCategory: "" };
+
+  changeCategory = (category) => {
+    this.setState({ currentCategory: category.categoryName });
+  };
+
+  render() {
+    return (
+      <div>
+        <Container>
+          <Row>
+            <Navi />
+          </Row>
+          <Row>
+            <Col xs="3">
+              <CategoryList
+                currentCategory={this.state.currentCategory}
+                changeCategory={this.changeCategory}
+                info={this.categoryInfo}
+              />
+            </Col>
+            <Col xs="9">
+              <ProductList currentCategory={this.state.currentCategory} info={this.productInfo} />
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    );
+  }
 }
-
-export default App;
--- src/CategoryList.js
+++ src/CategoryList.js
@@ -2,29 +2,53 @@ import React, { Component } from "react";
 import { ListGroup, ListGroupItem } from "reactstrap";
 
 export default class CategoryList extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      categories: [
-        { categoryId: 1, categoryName: "Beverages" },
-        { categoryId: 2, categoryName: "Condiments" },
-      ],
-    };
+  // constructor(props) {
+  //this.props.xxx.xxx Category Listte oluşturulan ctor ile aynı işleve sahip yani ctor yazmaya gerek yok.
+  //aynı durum state için de geçerli ornekteki gibi de kullanabilirdik ctor olmadan
+  //ORNEK:
+  // state = {
+  //   categories: [
+  //     { categoryId: 1, categoryName: "Beverages" },
+  //     { categoryId: 2, categoryName: "Condiments" },
+  //   ],
+  // };
+
+  //   super(props);
+  //   this.state = {
+  //     categories: [
+  //       { categoryId: 1, categoryName: "Beverages" },
+  //       { categoryId: 2, categoryName: "Condiments" },
+  //     ],
+  //   };
+  // }
+
+  state = {
+    categories: [
+      { categoryId: 1, categoryName: "Beverages" },
+      { categoryId: 2, categoryName: "Condiments" },
+    ],
+  };
+
+  changeCategory = (category) =>{
+    this.setState({currentCategory: category.categoryName})
   }
 
   render() {
     return (
       <div>
         <h3>{this.props.info.title}</h3>
-        <h3>{this.state.counter}</h3>
         <ListGroup>
           {//JS çalıştıracağım için bu parantezi açtım.
           this.state.categories.map((category) => (
-            <ListGroupItem key={category.categoryId}>
+            <ListGroupItem
+              onClick={() => this.props.changeCategory(category)}
+              key={category.categoryId}
+            >
               {category.categoryName}
             </ListGroupItem>
           ))}
         </ListGroup>
+        <h4>{this.props.currentCategory}</h4>
       </div>
     );
   }
