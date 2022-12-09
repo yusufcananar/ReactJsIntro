63d7b7b84711739c6884323468b0b84fdce586ec
Fjolnirr <yusufcan2617@gmail.com>
2022-12-09 16:04:40 +0300

    prop and state comments added.

--- src/ProductList.js
+++ src/ProductList.js
@@ -3,6 +3,7 @@ import React, { Component } from "react";
 export default class ProductList extends Component {
   
   //this.props.xxx.xxx Category Listte oluşturulan ctor ile aynı işleve sahip yani ctor yazmaya gerek yok.
+  //aynı durum state için de geçerli
   render() {
     return (
       <div>
